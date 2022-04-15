bd4cd89adc3e8aeef915967436c0e232c834e073
Chai Phonbopit <phonbopit@gmail.com>
2022-04-15 21:23:16 +0700

    Add test for transaction

--- token/test/index.ts
+++ token/test/index.ts
@@ -1,19 +1,42 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+const { utils } = ethers;
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('Greeter', function () {
+  describe('Transactions', function () {
+    it('should transfer tokens between accounts', async function () {
+      const [owner, wallet1, wallet2] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+      const Token = await ethers.getContractFactory('AhoyToken');
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+      const ahoyToken = await Token.deploy(100_000_000);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+      // Transfer 100 AHOY to wallet1
+      await ahoyToken.transfer(wallet1.address, utils.parseEther('100'));
+      expect(await ahoyToken.balanceOf(wallet1.address)).to.equal(
+        utils.parseEther('100')
+      );
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+      // Transfer 200 AHOY to wallet2
+      await ahoyToken.transfer(wallet2.address, utils.parseEther('200'));
+      expect(await ahoyToken.balanceOf(wallet2.address)).to.equal(
+        utils.parseEther('200')
+      );
+
+      // Wallet2 transfer 100 AHOY back to wallet1
+      await ahoyToken
+        .connect(wallet2)
+        .transfer(wallet1.address, utils.parseEther('100'));
+      expect(await ahoyToken.balanceOf(wallet2.address)).to.equal(
+        utils.parseEther('100')
+      );
+      expect(await ahoyToken.balanceOf(wallet1.address)).to.equal(
+        utils.parseEther('200')
+      );
+
+      expect(await ahoyToken.balanceOf(owner.address)).to.equal(
+        ethers.utils.parseEther('99999700')
+      );
+    });
   });
 });
